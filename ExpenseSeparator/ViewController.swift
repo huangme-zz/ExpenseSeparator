f575e2fcc092254f7b7b6bf4f6900b265d47abfc
Meng Huang <yuyeguaike@gmail.com>
2016-10-22 21:05:13 -0400

    Initial Commit

---
+++ ExpenseSeparator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ExpenseSeparator
+//
+//  Created by Meng Huang on 10/22/16.
+//  Copyright © 2016 Meng Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
